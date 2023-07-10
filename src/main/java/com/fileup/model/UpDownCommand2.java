68187b4ea2683b26663817dc10410cf406a286cf
nohssam73 <nohssam73@gmail.com>
2023-07-10 14:56:21 +0900

    202307010

--- src/main/java/com/fileup/model/UpDownCommand2.java
+++ src/main/java/com/fileup/model/UpDownCommand2.java
@@ -5,13 +5,8 @@ import java.io.BufferedOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.net.URLEncoder;
-import java.text.SimpleDateFormat;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
-import com.oreilly.servlet.MultipartRequest;
-import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 public class UpDownCommand2 {
 	FileInputStream fis = null;
